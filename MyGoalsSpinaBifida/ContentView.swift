0e7bbb2170df49e0c92546b5e521c4f222d95645
msheley <msheley@iu.edu>
2022-06-01 16:17:08 -0400

    Initial Commit

---
+++ MyGoalsSpinaBifida/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyGoalsSpinaBifida
+//
+//  Created by Sheley, Meena Ellen on 6/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyGoalsSpinaBifida/MyGoalsSpinaBifidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGoalsSpinaBifidaApp.swift
+//  MyGoalsSpinaBifida
+//
+//  Created by Sheley, Meena Ellen on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MyGoalsSpinaBifidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
